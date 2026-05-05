28259cc8b72d99b8d6a081d132c9888ac781595f
Josue Rojas <josuerojas.rojas@gmail.com>
2026-05-04 22:18:49 -0400

    path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const basePath = process.env.VITE_BASE_PATH ?? '/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/wedding/' : '/',
+  base: basePath,
   plugins: [react()],
   assetsInclude: ['**/*.heic', '**/*.HEIC'],
   server: {
