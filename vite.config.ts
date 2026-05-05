df880ee8557be1ffd5e255226a54d72491bf0d13
Josue Rojas <josuerojas.rojas@gmail.com>
2026-05-04 22:14:56 -0400

    path

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_PAGES === 'true' ? '/wedding/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.heic', '**/*.HEIC'],
-  server: {  
-    host: true  
-}
+  server: {
+    host: true,
+  },
 })
